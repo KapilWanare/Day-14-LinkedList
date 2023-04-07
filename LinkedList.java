e2d7f03fc7d398b61d82ebb7119f85e889f03ee3
Kapil Wanare <123574448+KapilWanare@users.noreply.github.com>
2023-04-07 21:13:59 +0530

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,60 @@
+public class LinkedList<T> {
+    Node<T> head;
+    Node<T> tail;
+
+    public void add( T data) {
+        Node<T> newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void show() {
+        Node<T> temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public void append(T data) {
+        Node<T> newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public T pop() {
+        if (head == null)
+            return null;
+        T popData = head.data;
+        head = head.next;
+        return popData;
+    }
+
+    public T popLast() {
+        if (head == null)
+            return null;
+        T popData = tail.data;
+        if (head == tail) {
+            head = null;
+            return popData;
+        }
+        Node<T> temp = head;
+        while (temp.next != tail) {
+            temp = temp.next;
+        }
+        temp.next = null;
+        tail = temp;
+        return popData;
+    }
+}
