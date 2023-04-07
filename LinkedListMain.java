aac972a30b73786af978415a1fd9401ca87d71be
Kapil Wanare <123574448+KapilWanare@users.noreply.github.com>
2023-04-07 21:15:22 +0530

    Create LinkedListMain.java

---
+++ LinkedListMain.java
@@ -0,0 +1,51 @@
+public class LinkedListMain {
+    public static void main(String[] args) {
+        //uc1
+        LinkedList<Integer> linkedList1 = new LinkedList<>();
+        //uc2
+        linkedList1.add(70);
+        linkedList1.add(30);
+        linkedList1.add(56);
+        linkedList1.show();
+
+        //uc3
+        LinkedList<Integer> linkedList2 = new LinkedList<>();
+        linkedList2.append(56);
+        linkedList2.append(30);
+        linkedList2.append(70);
+        linkedList2.show();
+
+
+        //uc5
+        Integer poppedData1 = linkedList1.pop();
+        if (poppedData1 == null)
+            System.out.println("Linked List is Empty");
+        else
+            System.out.println("The element popped is => " + poppedData1);
+
+        linkedList1.show();
+
+        //uc6
+        Integer poppedData2 = linkedList2.popLast();
+        if (poppedData2 == null)
+            System.out.println("Linked List is Empty");
+        else
+            System.out.println("The element popped is => " + poppedData2);
+
+        linkedList2.show();
+
+        //uc7
+        System.out.println("Adding Element Back to LinkedList");
+        linkedList2.add(70);
+        linkedList2.show();
+        System.out.println("Searching whether element 30 is present in LinkedList or not.");
+        Node<Integer> searchedData = linkedList2.search(30);
+        if (searchedData == null) {
+            System.out.println("Element 30 not found!");
+        } else {
+            System.out.println("Element 30 Found");
+        }
+    }
+
+
+}
