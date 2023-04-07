616a32c017567e94466056da0605c6a02bff9052
Kapil Wanare <123574448+KapilWanare@users.noreply.github.com>
2023-04-07 21:05:01 +0530

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,35 @@
+public class LinkedList<T> {
+    Node<T> head;
+    Node<T> tail;
+
+    public void add( T data) {
+        Node<T> newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void show() {
+        Node<T> temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public void append(T data) {
+        Node<T> newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+}
