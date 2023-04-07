59c479d20e21e7af980eba50c2f13d065cc19ce1
Kapil Wanare <123574448+KapilWanare@users.noreply.github.com>
2023-04-07 21:02:29 +0530

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,17 @@
+
+public class Node<T> {
+    T data;
+    Node<T> next;
+
+    public Node() {
+
+    }
+
+    public Node(T data){
+        this.data = data;
+    }
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
