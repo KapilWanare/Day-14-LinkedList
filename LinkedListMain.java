e6f6d8e5b2a7f646202d93e9432808e3a1ffd4fa
Kapil Wanare <123574448+KapilWanare@users.noreply.github.com>
2023-04-07 21:03:28 +0530

    Create LinkedListMain.java

---
+++ LinkedListMain.java
@@ -0,0 +1,9 @@
+public class LinkedListMain {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList1 = new LinkedList<>();
+        linkedList1.add(70);
+        linkedList1.add(30);
+        linkedList1.add(56);
+        linkedList1.show();
+    }
+}
